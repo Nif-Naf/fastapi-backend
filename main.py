6112c4f8dd90fe9ea1ba8a40c7ef9f0ec2d2a3aa
Nif-Naf <Nif-Naf94@yandex.ru>
2024-02-27 00:28:50 +0300

    Базовая конфигурация проекта.

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+# Check requirements .env file.
+env_conf = load_dotenv()
+if not env_conf:
+    raise FileNotFoundError("File .env not found.")
+
+# Initialization all variables.
+HOST = os.getenv("FA_HOST")
+PORT = int(os.getenv("FA_PORT"))
+DEBUG = bool(os.getenv("FA_DEBUG"))
+LOG_LEVEL = os.getenv("FA_LOG_LEVEL")
+RELOAD = bool(os.getenv("FA_AUTO_RELOAD"))
+
+# Create instance FastAPI.
+app = FastAPI(debug=DEBUG)
+
+
+@app.get(path="/")
+def health_checker():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="__main__:app",
+        host=HOST,
+        port=PORT,
+        log_level=LOG_LEVEL,
+        reload=RELOAD,
+    )
