29a8e35f78729a1a96dddc5587c2f1bb0794e544
Nif-Naf <Nif-Naf94@yandex.ru>
2024-03-04 11:40:18 +0300

    Добавление необходимых настроек.

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 import os
 
 from dotenv import load_dotenv
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 # Check requirements .env file.
 env_conf = load_dotenv()
@@ -8,11 +10,24 @@ if not env_conf:
     raise FileNotFoundError("File .env not found.")
 
 # Project settings.
-HOST = os.getenv("FA_HOST")
-PORT = int(os.getenv("FA_PORT"))
-DEBUG = bool(os.getenv("FA_DEBUG"))
-LOG_LEVEL = os.getenv("FA_LOG_LEVEL")
-RELOAD = bool(os.getenv("FA_AUTO_RELOAD"))
+HOST = os.getenv("HOST")
+PORT = int(os.getenv("PORT"))
+DEBUG = bool(os.getenv("DEBUG"))
+LOG_LEVEL = os.getenv("LOG_LEVEL")
+RELOAD = bool(os.getenv("AUTO_RELOAD"))
+
+# Auth settings.
+AUTH_PREFIX = "/api/v1/auth"
+OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl=AUTH_PREFIX + "/login")
+PASSWORD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALGORITHM = os.getenv("ALGORITHM")
+EXPIRATION = {
+    "days": int(os.getenv("ACCESS_TOKEN_EXPIRE_DAYS", 0)),
+    "hours": int(os.getenv("ACCESS_TOKEN_EXPIRE_HOURS", 1)),
+    "minutes": int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 0)),
+}
 
 # Session settings.
 SESSION_SETTINGS = {
